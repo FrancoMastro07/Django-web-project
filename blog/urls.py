0a0198cae70eb20b8ab25c23b6b16aff896590e2
FrancoMastro07 <francomastro07@gmail.com>
2023-12-31 14:50:47 -0300

    Blog app finished, i have to fix some things

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name="Blog"),
+    path('category/<int:category_id>/', views.category, name="Category"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
