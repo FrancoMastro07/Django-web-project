26ffc633aeb7231d140118e56ffce755f9769e8b
FrancoMastro07 <francomastro07@gmail.com>
2024-01-07 18:11:28 -0300

    Created authentication app

--- webProject/settings.py
+++ webProject/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'contact',
     'store',
     'cart',
+    'authentication',
 ]
 
 MIDDLEWARE = [
--- webProject/urls.py
+++ webProject/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('store/', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('authentication/', include('authentication.urls')),
 ]
