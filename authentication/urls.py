8445627c43a76a4ff6dd4363749e9936f3b16688
FrancoMastro07 <francomastro07@gmail.com>
2024-01-10 15:14:35 -0300

    Created log-in form and button

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ViewRegistration, log_out
+from .views import ViewRegistration, log_out, log_in
 
 urlpatterns = [
     path('', ViewRegistration.as_view(), name="Authentication"),
     path('log_out', log_out, name="Logout"),
+    path('log_in', log_in, name="Login"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 
@@ -28,3 +28,17 @@ def log_out(request):
     
     logout(request)
     return redirect('Home')
+
+def log_in(request):
+    
+    if request.method=="POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('Home')
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form":form})
