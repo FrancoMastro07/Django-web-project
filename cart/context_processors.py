cf470f60ec40d6e324b343ba96406ac62f735762
FrancoMastro07 <francomastro07@gmail.com>
2024-01-10 20:44:44 -0300

    Fixed some things

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,7 +1,8 @@
 def total_value(request):
     total = 0
-    #if request.user.is_authenticated:
-    '''
-    for key, value in request.session["cart"].items():
-        total += float(value["price"])'''
+    if request.user.is_authenticated:
+        for key, value in request.session["cart"].items():
+            total += float(value["price"])
+    else:
+        total = "You have to log-in to see the shopping cart"
     return {"total_value":total}
--- webProjectApp/views.py
+++ webProjectApp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from cart.cart import Cart
 
 def home(request):
+    
+    cart = Cart(request)
     return render(request, "webProjectApp/home.html")
 
 
